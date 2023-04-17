ab64552a9b8043830092eb0facea551885a54c0c
00hello <445986+00hello@users.noreply.github.com>
2023-04-17 23:23:44 +0000

    addded depth map that puts poses in their correct 3d world positions

--- examples/python/mp_pose/main.py
+++ examples/python/mp_pose/main.py
@@ -16,7 +16,8 @@ import requests
 import rerun as rr
 
 from scipy.spatial.transform import Rotation as R
-
+import depth
+import utilio
 
 EXAMPLE_DIR: Final = Path(os.path.dirname(__file__))
 DATASET_DIR: Final = EXAMPLE_DIR / "dataset" / "pose_movement"
@@ -44,14 +45,38 @@ def track_pose(video_path: str, segment: bool) -> None:
     #     "video/mask",
     #     [rr.AnnotationInfo(id=0, label="Background"), rr.AnnotationInfo(id=1, label="Person", color=(0, 0, 0))],
     # )
+
+    rr.log_annotation_context(
+        "/",
+        rr.ClassDescription(
+            info=rr.AnnotationInfo(label="Person"),
+            keypoint_annotations=[rr.AnnotationInfo(id=lm.value, label=lm.name) for lm in mp_pose.PoseLandmark],
+            keypoint_connections=mp_pose.POSE_CONNECTIONS,
+        ),
+    )
+
+    rr.log_view_coordinates("3dpeople", up="-Y", timeless=True)
+
+    intrinsics = get_camera_intrinsic_matrix()
+
+    # primary_camera_from_world = get_camera_pose_from_world(primary=True)
+    # rr.log_rigid3(
+    #     "camera0",
+    #     parent_from_child=primary_camera_from_world,
+    #     timeless=True,
+    # )
+
+    # rr.log_pinhole(
+    #     "camera0/image",
+    #     child_from_parent=intrinsics,
+    #     width=1280,
+    #     height=720,
+    #     timeless=True,
+    # )
+
     
-    rr.log_view_coordinates("/", up="-Y", timeless=True)
-
-    # rr.log_cleared("world/camera")
-
     # It's a non-moving camera so it doesn't go in the for loop
-    camera_from_world = get_camera_pose_from_world()
-
+    camera_from_world = get_camera_pose_from_world(primary=True)
 
     rr.log_rigid3(
         "camera",
@@ -59,7 +84,7 @@ def track_pose(video_path: str, segment: bool) -> None:
         timeless=True,
     )
 
-    intrinsics = get_camera_intrinsic_matrix()
+
     # Log camera intrinsics
     rr.log_pinhole(
         "camera/image",
@@ -69,14 +94,18 @@ def track_pose(video_path: str, segment: bool) -> None:
         timeless=True,
     )
 
-    rr.log_annotation_context(
-        "/",
-        rr.ClassDescription(
-            info=rr.AnnotationInfo(label="People"),
-            keypoint_annotations=[rr.AnnotationInfo(id=lm.value, label=lm.name) for lm in mp_pose.PoseLandmark],
-            keypoint_connections=mp_pose.POSE_CONNECTIONS,
-        ),
+    rr.log_pinhole(
+        "camera/depth",
+        child_from_parent=intrinsics,
+        width=1280,
+        height=720,
+        timeless=True,
     )
+    
+
+    
+    
+
     
     with closing(VideoSource(video_path)) as video_source:
             
@@ -86,11 +115,21 @@ def track_pose(video_path: str, segment: bool) -> None:
             rr.set_time_sequence("frame_idx", bgr_frame.idx)
             rr.log_image("camera/image", rgb) # don't put camera/image/rgb or it won't show the keypoints
 
-
             h, w, _ = rgb.shape
 
             # use yolov5 to detect person in the frame
             yolo_result = yolo_model(rgb)
+            depth_map = depth.run(rgb)
+
+            print("depth_map", depth_map)
+            print("depth_map.shape", depth_map.shape)
+            print("depth_map[0,0]", depth_map[0,0])
+
+            # rgb_depth = cv.imread("depthmap.png")
+            depth_image = utilio.depth_to_numpy_array(depth_map)
+
+            rr.log_image("camera/depth", depth_image) # don't put camera/image/rgb or it won't show the keypoints
+            
             person_id = 0
             for (xmin, ymin, xmax,   ymax,  confidence,  clas) in yolo_result.xyxy[0].tolist():
                 with mp_pose.Pose() as pose:
@@ -98,14 +137,18 @@ def track_pose(video_path: str, segment: bool) -> None:
                     # take each detected person bounding box, crop the original image to the bounding box and have mediapipe detect the pose in the crop
                     results = pose.process(rgb[int(ymin)+MARGIN:int(ymax)+MARGIN,int(xmin)+MARGIN:int(xmax)+MARGIN:])
 
-                    landmark_positions_2d = read_landmark_positions_2d(results, w, h, (xmin, ymin, xmax, ymax))
-                    rr.log_points("camera/image/people/"+str(person_id)+"/pose/keypoints", landmark_positions_2d, keypoint_ids=mp_pose.PoseLandmark)
-
-                    landmark_positions_3d = read_landmark_positions_3d(results, (xmin, ymin, xmax, ymax))
-                    rr.log_points("people/"+str(person_id)+"/pose/keypoints", landmark_positions_3d, keypoint_ids=mp_pose.PoseLandmark)
+                    # print("person_id", person_id)
+                    landmark_positions_2d = read_landmark_positions_2d(results, depth_map, w, h, (xmin, ymin, xmax, ymax))
+                    rr.log_points("camera/image/2dpeople/"+str(person_id)+"/pose/keypoints", landmark_positions_2d, keypoint_ids=mp_pose.PoseLandmark)
 
 
-                    # move_land_mark_positions_3d(person_id, results)
+                    rr.log_points("camera/depth/2dpeople/"+str(person_id)+"/pose/keypoints", landmark_positions_2d, keypoint_ids=mp_pose.PoseLandmark)
+
+                    landmark_positions_3d = read_landmark_positions_3d(results, landmark_positions_2d, depth_map, (xmin, ymin, xmax, ymax))
+                    rr.log_points("3dpeople/"+str(person_id)+"/pose/keypoints", landmark_positions_3d, keypoint_ids=mp_pose.PoseLandmark)
+
+
+                    # move_landmark_positions_3d(person_id, results, landmark_positions_2d)
 
                     # segmentation_mask = results.segmentation_mask
                     # if segmentation_mask is not None:
@@ -116,6 +159,7 @@ def track_pose(video_path: str, segment: bool) -> None:
 
 def read_landmark_positions_2d(
     results: Any,
+    depth_map: Any,
     image_width: int,
     image_height: int,
     bbox: tuple,
@@ -131,27 +175,85 @@ def read_landmark_positions_2d(
         # the normalized_landmarks are normalized to the cropped image, so we need to scale them back to the original image
         bbox_width = bbox[2]-bbox[0]
         bbox_height = bbox[3]-bbox[1]
-        return np.array(
-            [((bbox_width * lm.x) + bbox[0], (bbox_height * lm.y) + bbox[1], -(lm.z + 1.0) * 300.0) for lm in normalized_landmarks]
-        )
+        #depth_map[(bbox_width * lm.x) + bbox[0], (bbox_height * lm.y) + bbox[1]]
+
+        pixel_landmarks = []
+        idx = 0
+        for lm in normalized_landmarks:
+            x = (bbox_width * lm.x) + bbox[0]
+            y = (bbox_height * lm.y) + bbox[1]
+            z = 0
+                
+            pixel_landmarks.append([x, y, z])
+            idx += 1
+            
+        return np.array(pixel_landmarks)
+    
+        # return np.array(
+        #     [((bbox_width * lm.x) + bbox[0], (bbox_height * lm.y) + bbox[1], -(lm.z + 1.0) * 300.0) for lm in normalized_landmarks]
+        # )
+
 
 
 def read_landmark_positions_3d(
     results: Any,
+    landmark_positions_2d: Any,
+    depth_map: Any,
     bbox: tuple,
 ) -> Optional[npt.NDArray[np.float32]]:
     if results.pose_landmarks is None:
         return None
     else:
-        landmarks = [results.pose_world_landmarks.landmark[lm] for lm in mp.solutions.pose.PoseLandmark]
+
+        world_landmarks = [results.pose_world_landmarks.landmark[lm] for lm in mp.solutions.pose.PoseLandmark]
+
+        # the pixel landmarks and find the corresponding depth value
+        # for each 2d landmark, find the corresponding depth value
+            
+        # get depth of pixel at left hip
+        # print("hip.x", landmark_positions_2d[23,0])
+        # print("hip.y", landmark_positions_2d[23,1])
+        depth_left_hip = depth_map[int(landmark_positions_2d[23,1]), int(landmark_positions_2d[23,0])]
+        # print("depth_left_hip", depth_left_hip)
+
+        
+        # Intrinsic matrix K
+        K = get_camera_intrinsic_matrix()
+        
+        pixel_landmarks = []
+        idx = 0
+        for lm in world_landmarks:
+
+            # Z coordinate in the 3D world space. Adjusted according to the left hip depth
+            lm.z = depth_left_hip + lm.z
+
+            u = landmark_positions_2d[idx,0]
+            v = landmark_positions_2d[idx,1]
+            # 2D image coordinates (u, v)
+            image_coordinates = np.array([u, v, 1])
+            
+            # Compute the 3D world space coordinates
+            K_inv = np.linalg.inv(K)
+
+            homogeneous_world_coordinates = lm.z * K_inv @ image_coordinates
+            X_world, Y_world, _ = homogeneous_world_coordinates
+
+            lm.x = X_world
+            lm.y = Y_world
+                
+            pixel_landmarks.append([lm.x, lm.y, lm.z])
+            # print("idx", idx)
+            idx += 1
 
         # print("world landmarks", landmarks[0])
-        return np.array([(lm.x, lm.y, lm.z) for lm in landmarks])
+        return np.array([(lm.x, lm.y, lm.z) for lm in world_landmarks])
 
 
-def get_camera_pose_from_world():
+def get_camera_pose_from_world(primary=False):
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.transform.Rotation.html
     translation_list = [-48.17233535998567,0.8712905040130878,52.1138980103538] # scale is 1 = 3.08 cm
+    if primary:
+        translation_list = [0.0,0.0,0.0]
     # convert translation list values to meters at scale 1 = 0.00308 m
     translation_list = [x * 0.00308 for x in translation_list]
     # convert to numpy array
@@ -163,6 +265,9 @@ def get_camera_pose_from_world():
         [-0.039333797430330386, 0.9716767143068481, -0.23301762863259518],
         [-0.9753917438447207, 0.01329264436285671, 0.2200778308812537]
     ]
+
+    if primary:
+        rotation_nested_list = [1.0, 0.0, 0.0 ],[0.0, 1.0, 0.0],[0.0, 0.0, 1.0]
 
     rotation_array = np.array(rotation_nested_list)
     r = R.from_matrix(rotation_array)
@@ -179,6 +284,11 @@ def get_camera_intrinsic_matrix():
         [0, 856.6579127840464, 360.2700796586361],
         [0, 0, 1]
     ]
+    # intrinsic_list = [
+    #     [500, 0, 100],
+    #     [0, 500, 100],
+    #     [0, 0, 1]
+    # ]
 
     # convert to numpy array
     intrinsic_array = np.array(intrinsic_list)
@@ -187,12 +297,11 @@ def get_camera_intrinsic_matrix():
 
 
 
-    
 
-
-def move_land_mark_positions_3d(
+def move_landmark_positions_3d(
         person_id: int,
-        results: Any
+        results: Any,
+        landmark_positions_2d: npt.NDArray[np.float32],
     ) -> None:
     # the landmark positions are person coordinates, so we need to move them to the world coordinates
     # take the metric distrance between person's left and right hip
@@ -204,6 +313,19 @@ def move_land_mark_positions_3d(
     print("Person "+str(person_id)+" world:right hip", results.pose_world_landmarks.landmark[24])
 
     # The left hip and the right hip are on both sides of the origin. Add the absolute value of the left hip and the absolute value of the right hip to get the total width of the person.
+
+    # get the pixel position of the center point (between hips)
+
+    
+    # first get the absolute value of the left hip and the absolute value of the right hip
+    left_hip_x = abs(results.pose_landmarks.landmark[23].x)
+    left_hip_y = abs(results.pose_landmarks.landmark[23].y)
+    right_hip_x = abs(results.pose_landmarks.landmark[24].x)
+    right_hip_y = abs(results.pose_landmarks.landmark[24].y)
+
+    
+    
+    
 
     
     
--- examples/python/mp_pose/reload.sh
+++ examples/python/mp_pose/reload.sh
@@ -3,18 +3,16 @@
 # Activate conda environment
 source activate rerun_io_4_gln
 
-# Set the path to the Python file you want to monitor
-FILE_TO_MONITOR="main.py"
 
 # Define the command to run the Python file
-COMMAND_TO_RUN="python3 $FILE_TO_MONITOR --video_path /home/ubuntu/projects/raspberrypi3b_4_22.jpg --connect"
+COMMAND_TO_RUN="python3 main.py --video_path /home/ubuntu/projects/raspberrypi3b_4_sec.mp4 --connect"
 
 # Run the command initially
 $COMMAND_TO_RUN
 
-# Monitor the file for changes and re-run the command when a change is detected
+# Monitor all .py files in the current directory for changes and re-run the command when a change is detected
 while true; do
-    inotifywait -e modify $FILE_TO_MONITOR
+    inotifywait -q -e modify --exclude '.*\.swp' --exclude '.*\.swx' $(find . -name '*.py')
     $COMMAND_TO_RUN
 done
 
