83be5bbcb91d7a888c7e5ebc914428099bd9a431
00hello <445986+00hello@users.noreply.github.com>
2023-05-03 17:09:30 +0000

    Make reload.sh more likely to respond to ctrl-c

--- examples/python/mp_pose/reload.sh
+++ examples/python/mp_pose/reload.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# Activate conda environment
-source activate rerun_io_4_gln
-
-
 # Define the command to run the Python file
-COMMAND_TO_RUN="python3 main.py --video_path $1 --connect"
+COMMAND_TO_RUN="python3 main.py --video_path \$1 --connect"
+
+# Define a function to run when the script is interrupted
+function cleanup {
+    echo "Cleaning up..."
+    exit
+}
+
+# Set the trap to catch the interrupt signal and run the cleanup function
+trap cleanup INT
 
 # Run the command initially
 $COMMAND_TO_RUN
 
 # Monitor all .py files in the current directory for changes and re-run the command when a change is detected
 while true; do
-    inotifywait -q -e modify --exclude '.*\.swp' --exclude '.*\.swx' $(find . -name '*.py')
+    inotifywait -q -e modify --exclude '.*\.swp' --exclude '.*\.swx' $(find . -name '*.py') || true
     $COMMAND_TO_RUN
 done
-
